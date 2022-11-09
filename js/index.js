271ad60847773fd1d7d627d10495e65df75b57cd
unknown <mfoysal314@gmail.com>
2022-11-10 01:57:53 +0600

    database connected

--- js/index.js
+++ js/index.js
@@ -1,46 +1,28 @@
 
-
-let slidebtns = document.querySelector("body > div.page-two.container-fluid > div > div.click-flex > div");
-
-slidebtns.addEventListener('click',(x)=>{
-    if(x.target.classList=='others'){
-        let item = x.target;
-        let items = item.parentElement.children;
-
-        for(let i = 0 ; i < items.length;i++){
-            items[i].className = "others";
-        }
-        item.className = "active";
+const dis = document.getElementById("catagory");
+fetch("../database/database.json")
+  .then(res => res.json())
+  .then(data => {
+    for (let i = 0; i <= data.length; i++) {
+      dis.innerHTML += `
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top"
+        src="${data[i].image}"
+        alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">${data[i].name}</h5>
+        <p class="card-text">${data[i].description}
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+`
     }
-})
+  })
 
-const slider = document.querySelector(".horizontal-slider");
-let isDown = false;
-let startX;
-let scrollLeft;
 
-slider.addEventListener("mousedown", (e) => {
-  isDown = true;
-  slider.classList.add("active");
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener("mouseleave", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
-slider.addEventListener("mouseup", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
-slider.addEventListener("mousemove", (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3; //scroll-fast
-  slider.scrollLeft = scrollLeft - walk;
-  console.log(walk);
-});
+
+
 
 
 
